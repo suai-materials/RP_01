295d1f0b623fbba95acdafc88436482bbada7cd1
user <frostkslo1@yandex.ru>
2022-06-18 16:18:46 +0300

    ok

--- 3_C++/build-integrals-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/MinGW/bin/gcc.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "STRING" "" FORCE)
